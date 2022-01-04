675b6bb9880b7a510d10112d5b7efe8884970488
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2022-01-04 11:56:50 -0800

    Update server.js

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(session(sess));
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-
+app.set("port", PORT);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
