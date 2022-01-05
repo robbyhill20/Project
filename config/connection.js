f1888f46e5b9665589b65eb2f41aaa3d5d7b1110
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2022-01-04 20:06:29 -0800

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,16 @@ const Sequelize = require('sequelize')
 require('dotenv').config();
 
 let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
+if (process.env.NODE_ENV === "production") {
+  sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
-  
   sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_NAME ||
+    process.env.DB_Password
+    "",
     {
-      host: 'localhost',
-      dialect: 'mysql',
-      port:'process.env'
+      host: process.env.PSQL_HOST || "localhost",
+   
     }
   );
 }
